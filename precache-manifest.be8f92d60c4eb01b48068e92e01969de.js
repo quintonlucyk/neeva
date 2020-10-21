1c150f8fee26d89f50736d13306edb641a9ae19f
quintonlucyk <quinton_lucyk@yahoo.com>
2020-10-21 18:32:47 -0400

    Updates

---
+++ precache-manifest.be8f92d60c4eb01b48068e92e01969de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b912b990cd713f7c45541bafc2af429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e790872bcb0b6b5f0eff",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "df5a88564a583b409044",
+    "url": "/static/js/2.54f25971.chunk.js"
+  },
+  {
+    "revision": "1ab7518176c3809a2daa7d118a6f72b9",
+    "url": "/static/js/2.54f25971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e790872bcb0b6b5f0eff",
+    "url": "/static/js/main.d4afa6d8.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
